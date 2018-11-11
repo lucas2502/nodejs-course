f3f7d8f208e41361592404190777fd4fc5db250c
lucas2502 <lucas2502@live.com>
2018-11-11 03:03:54 +0000

    Start project chat real tome

---
+++ portalDeNoticias/app/routes/noticias.js
@@ -0,0 +1,11 @@
+//var dbConnection = require('../../config/dbConnection');
+
+module.exports = function(app){   
+    app.get('/noticias', function(req,res){
+        app.app.controllers.noticias.noticias(app, req, res);
+    });
+
+    app.get('/noticia', function(req, res){
+        app.app.controllers.noticias.noticia(app, req, res);
+    });
+};
