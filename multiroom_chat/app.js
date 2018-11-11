3ca21c303fcdedb27f9a92a270c4f373958911ec
lucas2502 <lucas2502@live.com>
2018-11-11 16:34:54 +0000

    config swap talk in chat

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -8,9 +8,27 @@ const server = app.listen(80, function(){
 
 const io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 // Creat connect websocket
 io.on('connection', function(socket){
+    console.log('Cliente conectou!')
+    socket.on('disconnect'), function(){
+        console.log("Cliente desconectou");
+    }
 
+    socket.on('msgParaServidor', function(data){
+        socket.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+    });
+    
 });
 
 
--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -5,11 +5,17 @@ module.exports.iniciaChat = function(application, req, res){
     req.assert('apelido','Nome ou apelido é obrigatorio').notEmpty();
     req.assert('apelido','Nome ou apelido deve conter de 3 a 15 caractere').len(3, 15);
     
-        var erros = req.validationErrors();
-        if(erros){
-            res.render("index", {validacao : erros});
-            return;
-        }
-    res.render("chat");
+    var erros = req.validationErrors();
+    if(erros){
+        res.render("index", {validacao : erros});
+        return;
+    }
+
+    application.get('io').emit(
+        'msgParaCliente',
+        {apelido: dadosForm.apelido, mensagem: 'Acabou de entrar no chat!'}
+        );
+
+    res.render("chat", {apelido: dadosForm});
 };
 
