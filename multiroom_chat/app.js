77b5b997cbfb5ef0469a9fce118969c1e8c9d9e2
lucas2502 <lucas2502@live.com>
2018-11-11 15:35:11 +0000

    Creact connection websocket

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -2,8 +2,15 @@
 const app = require('./config/server');
 
 // Parametrizar porta de escuta
-app.listen(80, function(){
+const server = app.listen(80, function(){
     console.log('Server ON!');
+});
+
+const io = require('socket.io').listen(server);
+
+// Creat connect websocket
+io.on('connection', function(socket){
+
 });
 
 
--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -3,7 +3,7 @@ module.exports.iniciaChat = function(application, req, res){
     var dadosForm = req.body;
     
     req.assert('apelido','Nome ou apelido é obrigatorio').notEmpty();
-    req.assert('apelido','Nome ou apelido deve conter de 3 a 15 caractere').notEmpty(3, 15);
+    req.assert('apelido','Nome ou apelido deve conter de 3 a 15 caractere').len(3, 15);
     
         var erros = req.validationErrors();
         if(erros){
